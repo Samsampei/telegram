31ed8b3f0436697ffeee8e8fcf95a4fef97f0157
Samsampei <samsampei72@gmail.com>
2025-03-10 18:13:24 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -63,7 +63,8 @@ def main():
 
     # Avvia il server Flask
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
+
 
 # Avvia il bot
 if __name__ == "__main__":
